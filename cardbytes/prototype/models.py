76bbbd4dc09895b4867a6e98c276da1e7d83ca09
Ravikiran Gunale <ravi.gunale@gmail.com>
2016-03-02 21:03:53 +0530

    models created for merchant and offers

--- cardbytes/prototype/models.py
+++ cardbytes/prototype/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Merchant(models.Model):
+	name = models.CharField(max_length=200)
+
+class Offers(models.Model):
+	merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE)
+	cashback = models.IntegerField(default=0)
+	cashback_status = models.CharField(max_length=50, default='Unused')
