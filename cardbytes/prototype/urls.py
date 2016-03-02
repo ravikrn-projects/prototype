f595624591eeb42d2832c0a674f69aac5421407a
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2016-03-02 21:14:48 +0530

    apis added create_merchant, delete_merchant, get_merchants

--- cardbytes/prototype/urls.py
+++ cardbytes/prototype/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^create_merchant$', views.create_merchant, name='create_merchant'),
+    url(r'^delete_merchant$', views.delete_merchant, name='delete_merchant'),
+    url(r'^get_merchants$', views.get_merchants, name='get_merchants'),
 ]
--- cardbytes/prototype/views.py
+++ cardbytes/prototype/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from prototype.models import Merchant
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return HttpResponse("Hello, Welocome to Cardbytes Prototype.")
+
+def create_merchant(request):
+    name = request.GET['name']
+    try:
+        merchant = Merchant(name = name)
+        merchant.save()
+        response = {'success': True}
+    except Exception as e:
+        response = {'success': False, 'error': str(e)}
+    return JsonResponse(response)
+
+def delete_merchant(request):
+    id = request.GET['id']
+    try:
+        merchant = Merchant(id = id).delete()
+        response = {'success': True}
+    except Exception as e:
+        response = {'success': False, 'error': str(e)}
+    return JsonResponse(response)
+
+def get_merchants(request):
+    try:
+        merchants = Merchant.objects.all().values('id', 'name')
+        response = {'success': True, 'merchants': list(merchants)}
+    except Exception as e:
+        response = {'success': False, 'error': str(e)}
+    return JsonResponse(response)
