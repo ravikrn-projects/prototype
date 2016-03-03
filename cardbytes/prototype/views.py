43f3bd97a2304a037f8dfc5f6682750232c87a0d
Ravikiran Gunale <ravi.gunale@gmail.com>
2016-03-03 19:01:05 +0530

    minor

--- cardbytes/prototype/views.py
+++ cardbytes/prototype/views.py
@@ -85,7 +85,7 @@ def update_user(user_id, cashback, amount):
 
 def update_status(user_id, merchant_id, cashback):
     if cashback>0
-        offer = Offer.objects.filter(user_id=user_id, merchant_id=merchant_id)
+        offer = Offer.objects.get(user_id=user_id, merchant_id=merchant_id)
         offer.cashback_status = True
         offer.save()
 
