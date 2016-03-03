9e3e7261fdccdca12461f5847a86464c9f9a8afc
Ravikiran Gunale <ravi.gunale@gmail.com>
2016-03-03 16:47:41 +0530

    tabs to space

---
+++ cardbytes/prototype/config.py
@@ -0,0 +1,2 @@
+vendor_commission = 0.05
+bank_commisision = 0.01
--- cardbytes/prototype/views.py
+++ cardbytes/prototype/views.py
@@ -1,40 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from prototype.models import Offers, Merchant, Vendor, Bank, User
+from config import vendor_commision, bank_commision
 
 def index(request):
     return HttpResponse("Hello, Welocome to Cardbytes Prototype.")
 
 def generate_offers(request):
-	params = request.GET
-	try:
-		offers = Offers.objects.all().values('user_id', 'merchant_id', 'cashback', 'cashback_status')
-		offer_list = list(offers)
-		offer_list_response = []
-		for offer in offer_list:
-			offer['merchant'] = Merchant.objects.get(id = offer['merchant_id']).name
-			offer_list_response.append(offer)
-		response = {'success': True, 'offers': offer_list_response}
-	except Exception as e:
-		response = {'success': False, 'error': str(e)}
-	return JsonResponse(response)
+    params = request.GET
+    try:
+        offers = Offers.objects.all().values('user_id', 'merchant_id', 'cashback', 'cashback_status')
+        offer_list = list(offers)
+        offer_list_response = []
+        for offer in offer_list:
+            offer['merchant'] = Merchant.objects.get(id = offer['merchant_id']).name
+            offer_list_response.append(offer)
+        response = {'success': True, 'offers': offer_list_response}
+    except Exception as e:
+        response = {'success': False, 'error': str(e)}
+    return JsonResponse(response)
 
 def get_vendor_revenue(request):
-	try:
-		vendor = Vendor.objects.all()[0]
-		response = {'success': True, 'revenue': vendor.revenue}
-	except Exception as e:
-		response = {'success': False, 'error': str(e)}
-	return JsonResponse(response)
+    try:
+        vendor = Vendor.objects.all()[0]
+        response = {'success': True, 'revenue': vendor.revenue}
+    except Exception as e:
+        response = {'success': False, 'error': str(e)}
+    return JsonResponse(response)
 
 def get_bank_revenue(request):
-	try:
-		bank = Bank.objects.all()[0]
-		response = {'success': True, 'revenue_without_clm': bank.revenue_without_clm, 
-		'revenue_with_clm': bank.revenue_with_clm}
-	except Exception as e:
-		response = {'success': False, 'error': str(e)}
-	return JsonResponse(response)
+    try:
+        bank = Bank.objects.all()[0]
+        response = {'success': True, 'revenue_without_clm': bank.revenue_without_clm, 
+        'revenue_with_clm': bank.revenue_with_clm}
+    except Exception as e:
+        response = {'success': False, 'error': str(e)}
+    return JsonResponse(response)
 
 def get_merchants(request):
     try:
@@ -83,7 +84,9 @@ def get_cashback(user_id, merchant_id, amount):
     return 0
 
 def update_vendor(user_id, merchant_id, amount):
+    vendor_commision_amt = vendor_commision*amount
     pass
+
 
 def update_bank(user_id, merchant_id, amount):
     pass
