1fb666f6fdfdf0d3a6eff5bcf4d8013af02ea9ae
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2016-03-04 17:17:12 +0530

    dropdown working

--- cardbytes/prototype/static/js/customer.js
+++ cardbytes/prototype/static/js/customer.js
@@ -1,16 +1,15 @@
 
+var selected_merchant;
 function buy(){
-    console.log($("#amount").val());
     $.ajax(
             {
                 url: "http://127.0.0.1:8000/cardbytes/transact", 
                 data: {
                         user_id: user_id,
                         amount: $("#amount").val(),
-                        merchant_id: 48
+                        merchant_id: selected_merchant
                 },
                 success: function(result){
-                    console.log(result);
                     updateUserData();
                 }
             }
@@ -30,17 +29,25 @@ function updateUserData(){
     );
 }       
 
+function updateMerchant(merchant, id){
+    selected_merchant = id;
+    $("#merchant").html(merchant);
+}
+
 function updateMerchantData(){
     $.ajax(
             {
                 url: "http://127.0.0.1:8000/cardbytes/get_merchants", 
                 success: function(result){
-                    console.log(result);
                     var list_items = "";
-                    for(var i=0;i<result.merchants.length;i++)
-                        list_items += "<li><a href='#!'>"+ result.merchants[i].name +"</a></li>";
+                    for(var i=0;i<result.merchants.length;i++){
+                        var merchant = result.merchants[i];
+                        var onclick = "onClick=\"updateMerchant(" + "'"+ merchant.name + "'"+ ", "+ merchant.id    + ")\"";
+                        list_items += "<li><a href='#!' "+onclick+">"+ merchant.name +"</a></li>";
+                    }
                     $("#dropdown1").html(list_items);
                     $("#merchant").html(result.merchants[0].name);
+                    selected_merchant = result.merchants[0].id;
                 }
             }
         );
